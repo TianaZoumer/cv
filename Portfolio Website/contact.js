b80439fbe7c2498caaadc6c5e8711ac2b6a2e63d
TianaZoumer <115896875+TianaZoumer@users.noreply.github.com>
2022-12-16 13:02:26 -0500

    CV upload files

---
+++ Portfolio Website/contact.js
@@ -0,0 +1,28 @@
+
+// function validateForm() {
+//     // get the form element
+//     var form = document.getElementById("contact-form");
+
+//     // get the form input elements
+//     var nameInput = document.getElementById("name");
+//     var emailInput = document.getElementById("email");
+//     var messageInput = document.getElementById("message");
+
+//     // validate the form input elements
+//     if (nameInput.value == "") {
+//         alert("Please enter your name");
+//         return false;
+//     }
+//     if (emailInput.value == "") {
+//         alert("Please enter your email address");
+//         return false;
+//     }
+//     if (messageInput.value == "") {
+//         alert("Please enter a message");
+//         return false;
+//     }
+
+//     // form is valid, submit the form
+//     form.submit();
+// }
+
